275284e8753d1d4bac5acc96b92fcaf2da1a50b0
bethany.brown <bethany.brown@servicenow.com>
2017-10-31 14:54:50 -0700

    mobile navigation, image animation, etc

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
 
+	// trigger animation on header image
+	$('#header-img').addClass('active');
+
 	// custom file input selection, show file name
 	$('#file').on('change',function(){
-		var value = $(this).val().replace(/^.*\\/, "");
-	 	$(this).parent().find('#file-path').addClass("selected").text(value);
+		var value = $(this).val().replace(/^.*\\/, '');
+	 	$(this).parent().find('#file-path').addClass('selected').text(value);
 	})
 
+	// navigate to language specific page on select
+	$("#language").change(function(){
+	    if ($(this).val()!='') {
+	      window.location.href=$(this).val();
+	    }
+  	});
+
+	// hamburger navigation (keeping it simple for this project)
+	// to do, add a check for the active class, add aria-expanded
+	$('#hamburger').on('click', function(){
+		$(this).toggleClass('active');
+		$(this).parent().find('.nav').toggleClass('active');
+	})
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -21,12 +21,28 @@ var bootstrap=function(t,e,n){"use strict";function i(t,e){for(var n=0;n<e.lengt
 
 $(document).ready(function() {
 
+	// trigger animation on header image
+	$('#header-img').addClass('active');
+
 	// custom file input selection, show file name
 	$('#file').on('change',function(){
-		var value = $(this).val().replace(/^.*\\/, "");
-	 	$(this).parent().find('#file-path').addClass("selected").text(value);
+		var value = $(this).val().replace(/^.*\\/, '');
+	 	$(this).parent().find('#file-path').addClass('selected').text(value);
 	})
 
+	// navigate to language specific page on select
+	$("#language").change(function(){
+	    if ($(this).val()!='') {
+	      window.location.href=$(this).val();
+	    }
+  	});
+
+	// hamburger navigation (keeping it simple for this project)
+	// to do, add a check for the active class, add aria-expanded
+	$('#hamburger').on('click', function(){
+		$(this).toggleClass('active');
+		$(this).parent().find('.nav').toggleClass('active');
+	})
 });
 
 //# sourceMappingURL=main.js.map
